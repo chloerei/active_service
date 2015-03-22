b8e87738dfc860b48074e665e49cb0178e57596e
Rei <chloerei@gmail.com>
2015-03-22 14:51:20 +0800

    Add HelloService test case

--- test/active_service/base_test.rb
+++ test/active_service/base_test.rb
@@ -4,7 +4,22 @@ class ActiveService::BaseTest < Minitest::Test
   class FoobarService < ActiveService::Base
   end
 
+  class HelloService < ActiveService::Base
+    def initialize(name)
+      @name = name
+    end
+
+    def say_hello
+      "Hello #{@name}!"
+    end
+  end
+
   def test_should_inhert_base
     assert FoobarService.new
+  end
+
+  def test_hello_service
+    hello_service = HelloService.new('David')
+    assert_equal 'Hello David!', HelloService.new('David').say_hello
   end
 end
